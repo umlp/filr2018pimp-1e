4746e3a158e02cf42c907947c148633720f1fb94
qenjalbert <36072947+qenjalbert@users.noreply.github.com>
2018-05-24 14:56:42 +0200

    Ajout d'espaces

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,8 +45,8 @@
         <li><a href="#">Rechercher un stage</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span>S'inscrire</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>Se connecter</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> S'inscrire</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Se connecter</a></li>
       </ul>
     </div>
   </div>
