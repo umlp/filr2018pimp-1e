317fee0ebf528c6604a1ea8f77e306e1e82aa960
Enjalbert Quentin <quentin.enjalbert@insa-rouen.fr>
2018-06-07 16:27:20 +0200

    test bd

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,157 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="col-sm-offset-2 col-sm-8">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Les entreprises
-                </div>
-                <div class="panel-body">
-                    <table class="table table-striped task-table">
-                        <tbody>
+    <!-- Create Task Form... -->
+
+    <!-- Current Tasks -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- Table Headings -->
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- Table Body -->
+                    <tbody>
+                        @foreach ($tasks as $task)
                             <tr>
-                                <td class="table-text"><div>1</div></td>
-                                <td class="table-text"><div>Google</div></td>
-                                <td>
-                                    
+                                <!-- Task Name -->
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
                                 </td>
+
+                                <!-- Delete Button -->
                                 <td>
-                                    <button type="button" class="btn btn-danger">
-                                        <i class="fa fa-btn fa-sort-desc"></i>
-                                        Rank down
-                                    </button>
+                                    <form action="/task/{{ $task->id }}" method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+
+                                        <button>Delete Task</button>
+                                    </form>
                                 </td>
                             </tr>
-                            <tr>
-                                <td class="table-text"><div>2</div></td>
-                                <td class="table-text"><div>Netflix</div></td>
-                                <td>
-                                    <button type="button" class="btn btn-success">
-                                        <i class="fa fa-btn fa-sort-asc" aria-hidden="true"></i>
-                                        Rank up
-                                    </button>
-                                </td>
-                                <td>
-                                    <button type="button" class="btn btn-danger">
-                                        <i class="fa fa-btn fa-sort-desc"></i>
-                                        Rank down
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="table-text"><div>3</div></td>
-                                <td class="table-text"><div>Dassaut</div></td>
-                                <td>
-                                    <button type="button" class="btn btn-success">
-                                        <i class="fa fa-btn fa-sort-asc" aria-hidden="true"></i>
-                                        Rank up
-                                    </button>
-                                </td>
-                                <td>
-                                    <button type="button" class="btn btn-danger">
-                                        <i class="fa fa-btn fa-sort-desc"></i>
-                                        Rank down
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="table-text"><div>5</div></td>
-                                <td class="table-text"><div>Renault</div></td>
-                                <td>
-                                    <button type="button" class="btn btn-success">
-                                        <i class="fa fa-btn fa-sort-asc" aria-hidden="true"></i>
-                                        Rank up
-                                    </button>
-                                </td>
-                                <td>
-                                    <button type="button" class="btn btn-danger">
-                                        <i class="fa fa-btn fa-sort-desc"></i>
-                                        Rank down
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="table-text"><div>6</div></td>
-                                <td class="table-text"><div>Huawei</div></td>
-                                <td>
-                                    <button type="button" class="btn btn-success">
-                                        <i class="fa fa-btn fa-sort-asc" aria-hidden="true"></i>
-                                        Rank up
-                                    </button>
-                                </td>
-                                <td>
-                                    <button type="button" class="btn btn-danger">
-                                        <i class="fa fa-btn fa-sort-desc"></i>
-                                        Rank down
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="table-text"><div>7</div></td>
-                                <td class="table-text"><div>SNCF</div></td>
-                                <td>
-                                    <button type="button" class="btn btn-success">
-                                        <i class="fa fa-btn fa-sort-asc" aria-hidden="true"></i>
-                                        Rank up
-                                    </button>
-                                </td>
-                                <td>
-                                    <button type="button" class="btn btn-danger">
-                                        <i class="fa fa-btn fa-sort-desc"></i>
-                                        Rank down
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="table-text"><div>8</div></td>
-                                <td class="table-text"><div>Sopra Steria</div></td>
-                                <td>
-                                    <button type="button" class="btn btn-success">
-                                        <i class="fa fa-btn fa-sort-asc" aria-hidden="true"></i>
-                                        Rank up
-                                    </button>
-                                </td>
-                                <td>
-                                    <button type="button" class="btn btn-danger">
-                                        <i class="fa fa-btn fa-sort-desc"></i>
-                                        Rank down
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="table-text"><div>9</div></td>
-                                <td class="table-text"><div>Thales</div></td>
-                                <td>
-                                    <button type="button" class="btn btn-success">
-                                        <i class="fa fa-btn fa-sort-asc" aria-hidden="true"></i>
-                                        Rank up
-                                    </button>
-                                </td>
-                                <td>
-                                    <button type="button" class="btn btn-danger">
-                                        <i class="fa fa-btn fa-sort-desc"></i>
-                                        Rank down
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="table-text"><div>10</div></td>
-                                <td class="table-text"><div>Orange</div></td>
-                                <td>
-                                    <button type="button" class="btn btn-success">
-                                        <i class="fa fa-btn fa-sort-asc" aria-hidden="true"></i>
-                                        Rank up
-                                    </button>
-                                </td>
-                                <td>
-                                    
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
-    </div>
+    @endif
 @endsection
